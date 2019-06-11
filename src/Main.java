0dcbb037f9e5105cffacf4bd199345b84eee996f
Alexander Zakharov <ekzalt@gmail.com>
2019-06-11 21:22:39 +0300

    added thread array

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ import com.hillel.hw08.ScannerReader;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        System.out.println("\nhomework 7\n");
-        new Application().run();
-         */
-
         System.out.println("\nhomework 8\n");
         new ScannerReader().run();
     }
--- src/com/hillel/hw08/thread/ThreadSeparator.java
+++ src/com/hillel/hw08/thread/ThreadSeparator.java
@@ -5,6 +5,7 @@ import com.hillel.hw08.collection.Collector;
 public class ThreadSeparator {
     private int threads = 1;
     private Collector collector;
+    private int threadCounter;
 
     public ThreadSeparator(Collector collector, int threads) {
         this.collector = collector;
@@ -12,20 +13,28 @@ public class ThreadSeparator {
     }
 
     public void separate(int from, int to) throws InterruptedException {
-        int prepared = from > 1 ? from : 1;
         int batchSize = Math.round(to / threads);
+        Thread[] workers = new Thread[threads];
+        threadCounter = 0;
 
-        for (int i = prepared; i <= threads; i++) {
-            int start = (i * batchSize) - (batchSize - 1);
-            int finish = i * batchSize;
+        for (int i = threadCounter; i < threads; i++) {
+            int start = ((i+1) * batchSize) - (batchSize - 1);
+            int finish = (i+1) * batchSize;
 
             System.out.println("start: " + start);
             System.out.println("finish: " + finish);
 
-            Thread thread = new Thread(new ThreadWorker(collector, start, finish));
+            from = from > start ? from : start;
+            Thread thread = new Thread(new ThreadWorker(collector, from, finish));
+            workers[i] = thread;
             thread.start();
-            // thread.join();
         }
+
+        /*
+        for (Thread thread : workers) {
+            thread.join();
+        }
+         */
     }
 
     public int getThreads() {

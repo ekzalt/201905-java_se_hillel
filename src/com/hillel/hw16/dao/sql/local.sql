a9642221a9f39bd5b34bd54c2d5eb9eddb0f8336
Alexander Zakharov <ekzalt@gmail.com>
2019-08-04 17:54:43 +0300

    re-dump local db

--- src/com/hillel/hw16/dao/sql/local.sql
+++ src/com/hillel/hw16/dao/sql/local.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 03, 2019 at 11:00 AM
+-- Generation Time: Aug 04, 2019 at 02:52 PM
 -- Server version: 5.6.37
 -- PHP Version: 7.1.8
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `actors` (
   `born` date DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `actors`
@@ -46,7 +46,8 @@ INSERT INTO `actors` (`id`, `name`, `born`, `created_at`, `updated_at`) VALUES
 (5, 'Shaun Toub', '1963-04-06', '2019-07-29 18:36:09', '2019-07-29 18:36:09'),
 (6, 'Gwyneth Paltrow', '1972-09-27', '2019-07-29 18:36:09', '2019-07-29 18:36:09'),
 (7, 'Hugo Weaving', '1960-04-04', '2019-07-29 18:39:42', '2019-07-29 18:39:42'),
-(8, 'Samuel L. Jackson', '1948-12-21', '2019-07-29 18:39:42', '2019-07-29 18:39:42');
+(8, 'Samuel L. Jackson', '1948-12-21', '2019-07-29 18:39:42', '2019-07-29 18:39:42'),
+(9, 'Test Director Actor', '1980-01-07', '2019-08-04 11:52:36', '2019-08-04 11:52:36');
 
 -- --------------------------------------------------------
 
@@ -61,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `directors` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `film_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `directors`
@@ -69,7 +70,8 @@ CREATE TABLE IF NOT EXISTS `directors` (
 
 INSERT INTO `directors` (`id`, `name`, `born`, `created_at`, `updated_at`, `film_id`) VALUES
 (1, 'Jon Favreau', '1966-10-19', '2019-07-28 15:08:18', '2019-07-28 15:08:18', 1),
-(2, 'Joe Johnston', '1950-05-13', '2019-07-28 15:08:18', '2019-07-28 15:08:18', 2);
+(2, 'Joe Johnston', '1950-05-13', '2019-07-28 15:08:18', '2019-07-28 15:08:18', 2),
+(3, 'Test Director Actor', '1980-01-07', '2019-08-04 11:45:37', '2019-08-04 11:45:37', 3);
 
 -- --------------------------------------------------------
 
@@ -85,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `films` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `director_id` int(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `films`
@@ -93,7 +95,8 @@ CREATE TABLE IF NOT EXISTS `films` (
 
 INSERT INTO `films` (`id`, `name`, `country`, `released`, `created_at`, `updated_at`, `director_id`) VALUES
 (1, 'Iron Man', 'USA', '2008-04-14', '2019-07-28 13:44:50', '2019-07-28 13:44:50', 1),
-(2, 'Captain America: The First Avenger', 'USA', '2011-07-22', '2019-07-28 13:44:50', '2019-07-28 13:44:50', 2);
+(2, 'Captain America: The First Avenger', 'USA', '2011-07-22', '2019-07-28 13:44:50', '2019-07-28 13:44:50', 2),
+(3, 'Test film', 'USA', '2011-11-11', '2019-08-04 11:47:15', '2019-08-04 11:47:15', 3);
 
 -- --------------------------------------------------------
 
@@ -105,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `films_actors` (
   `id` int(11) NOT NULL,
   `film_id` int(11) DEFAULT NULL,
   `actor_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `films_actors`
@@ -119,7 +122,8 @@ INSERT INTO `films_actors` (`id`, `film_id`, `actor_id`) VALUES
 (5, 1, 5),
 (6, 1, 6),
 (7, 2, 7),
-(8, 2, 8);
+(8, 2, 8),
+(9, 2, 1);
 
 --
 -- Indexes for dumped tables
@@ -157,22 +161,22 @@ ALTER TABLE `films_actors`
 -- AUTO_INCREMENT for table `actors`
 --
 ALTER TABLE `actors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `directors`
 --
 ALTER TABLE `directors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `films`
 --
 ALTER TABLE `films`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `films_actors`
 --
 ALTER TABLE `films_actors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

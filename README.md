16f87296b2b991d13d2606e7ba10e462c5ed52c7
Alexander Zakharov <ekzalt@gmail.com>
2019-08-18 15:38:52 +0300

    fix build

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 ## Run application
 
 ```bash
+# run app once
 mvn exec:java
+
+# run app with jetty
+mvn jetty:run
 ```
--- src/main/java/com/hillel/Main.java
+++ src/main/java/com/hillel/Main.java
@@ -5,6 +5,6 @@ import com.hillel.hw16.VideoLibrary;
 public class Main {
     public static void main(String[] args) {
         System.out.println("\nhomework 16\n");
-        new VideoLibrary().run();
+        // new VideoLibrary().run();
     }
 }
